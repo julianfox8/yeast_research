4bee541ca32d617cd2d807a2d6bfc19bb141c686
julianfox8 <49963009+julianfox8@users.noreply.github.com>
2021-04-17 14:51:34 -0700

    Update run_all_queries_test.py

--- run_all_queries_test.py
+++ run_all_queries_test.py
@@ -5,14 +5,14 @@ import sys
 def XC_grabber(db):
     c = sqlite3.connect(db)
     cursor = c.cursor()
-    XC_result = [ row[0] for row in cursor.execute('select ParameterIndex,Vertex from Signatures natural join ( select MorseGraphIndex,Vertex from MorseGraphAnnotations where label="XC {x, z}" except select MorseGraphIndex,Source from MorseGraphEdges);')]
+    XC_result = [ row[0] for row in cursor.execute('select ParameterIndex from Signatures natural join ( select MorseGraphIndex,Vertex from MorseGraphAnnotations where label="XC {x, z}" except select MorseGraphIndex,Source from MorseGraphEdges);')]
     with open('XC_query_test.txt', 'w') as f:
         json.dump(XC_result,f)
 
 def FC_grabber(db):
     c = sqlite3.connect(db)
     cursor1 = c.cursor()
-    FC_result = [row[0] for row in cursor1.execute('select ParameterIndex, Vertex from Signatures natural join (select MorseGraphIndex,Vertex from (select MorseGraphIndex,Vertex from MorseGraphAnnotations where Label="FC" except select MorseGraphIndex,Source from MorseGraphEdges));')]
+    FC_result = [row[0] for row in cursor1.execute('select ParameterIndex from Signatures natural join (select MorseGraphIndex,Vertex from (select MorseGraphIndex,Vertex from MorseGraphAnnotations where Label="FC" except select MorseGraphIndex,Source from MorseGraphEdges));')]
     with open('FC_query_test.txt', 'w') as f:
         json.dump(FC_result,f)
 
@@ -30,14 +30,14 @@ def FP_grabber(db):
 def mono_grabber(db):
     c = sqlite3.connect(db)
     cursor3 = c.cursor()
-    monostable_result = [row[0] for row in cursor3.execute("select MorseGraphIndex from (select MorseGraphIndex, count(*) as StableCount from (select MorseGraphIndex,Vertex from MorseGraphVertices except select MorseGraphIndex,Source from MorseGraphEdges) group by MorseGraphIndex) where StableCount=1;")]
+    monostable_result = [row[0] for row in cursor3.execute("select ParameterIndex from Signatures natural join (select MorseGraphIndex, count(*) as StableCount from (select MorseGraphIndex,Vertex from MorseGraphVertices except select MorseGraphIndex,Source from MorseGraphEdges) group by MorseGraphIndex) where StableCount=1;")]
     with open('monostable_query_test.txt', 'w') as f:
         json.dump(monostable_result,f)
 
 def bistable_grabber(db):
     c = sqlite3.connect(db)
     cursor4 = c.cursor()
-    bistable_result = [row[0] for row in cursor4.execute("select MorseGraphIndex from (select MorseGraphIndex, count(*) as StableCount from (select MorseGraphIndex,Vertex from MorseGraphVertices except select MorseGraphIndex,Source from MorseGraphEdges) group by MorseGraphIndex) where StableCount=2;")]
+    bistable_result = [row[0] for row in cursor4.execute("select ParameterIndex from Signatures natural join (select MorseGraphIndex, count(*) as StableCount from (select MorseGraphIndex,Vertex from MorseGraphVertices except select MorseGraphIndex,Source from MorseGraphEdges) group by MorseGraphIndex) where StableCount=2;")]
     with open('bistable_query_test.txt', 'w') as f:
         json.dump(bistable_result,f)
 
